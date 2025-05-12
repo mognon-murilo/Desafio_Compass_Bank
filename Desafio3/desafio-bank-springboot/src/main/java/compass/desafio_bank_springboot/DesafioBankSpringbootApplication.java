e002523547a00a2102b9ff09c285b304888477c2
Murilo Mognon <mognonmmurilo@gmail.com>
2025-05-12 15:08:47 -0300

    projeto inicial

---
+++ Desafio3/desafio-bank-springboot/src/main/java/compass/desafio_bank_springboot/DesafioBankSpringbootApplication.java
@@ -0,0 +1,13 @@
+package compass.desafio_bank_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBankSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBankSpringbootApplication.class, args);
+	}
+
+}
---
+++ Desafio3/desafio-bank-springboot/src/test/java/compass/desafio_bank_springboot/DesafioBankSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package compass.desafio_bank_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBankSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
